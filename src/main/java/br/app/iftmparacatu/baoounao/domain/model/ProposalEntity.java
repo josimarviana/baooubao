d25e0b48f5e9f9975801a76706158b55951fc09f
ElisabeteXavier <elisabetebx@gmail.com>
2024-03-06 20:41:46 -0300

    adicionando modificador de acesso ProposalEntity

--- src/main/java/br/app/iftmparacatu/baoounao/domain/model/ProposalEntity.java
+++ src/main/java/br/app/iftmparacatu/baoounao/domain/model/ProposalEntity.java
@@ -8,25 +8,113 @@ import java.time.LocalDateTime;
 @Entity
 public class ProposalEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     @Column(length = 100)
-    String title;
+    private String title;
     @Column
-    String description;
+    private String description;
     @Column
-    Integer likes;
+    private Integer likes;
     @Column(length = 100)
-    String situation;
+    private String situation;
     @Column
-    Boolean active;
+    private Boolean active;
     @Column
-    LocalDateTime createdAt;
+    private LocalDateTime createdAt;
     @Column(length = 100)
-    String url;
+    private String url;
     @Column
-    Blob photograpy;
+    private  Blob photograpy;
 
-    //Usuario usuario;
-    //Ciclo ciclo;
+    //Usuario usuario; Ciclo ciclo;
+
+    public ProposalEntity(Long id, String title, String description, Integer likes, String situation, Boolean active, LocalDateTime createdAt, String url, Blob photograpy) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.likes = likes;
+        this.situation = situation;
+        this.active = active;
+        this.createdAt = createdAt;
+        this.url = url;
+        this.photograpy = photograpy;
+    }
+
+    public ProposalEntity() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getLikes() {
+        return likes;
+    }
+
+    public void setLikes(Integer likes) {
+        this.likes = likes;
+    }
+
+    public String getSituation() {
+        return situation;
+    }
+
+    public void setSituation(String situation) {
+        this.situation = situation;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Blob getPhotograpy() {
+        return photograpy;
+    }
+
+    public void setPhotograpy(Blob photograpy) {
+        this.photograpy = photograpy;
+    }
+
 }
