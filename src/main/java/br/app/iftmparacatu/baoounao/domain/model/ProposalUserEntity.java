bdc05d3e327f03e9d545bff6e8688a98955e7076
Felipe Soares <soares.felipe200@gmail.com>
2024-03-06 20:17:55 -0300

    feat: adicionando classe associativa entre usuario e proposta

---
+++ src/main/java/br/app/iftmparacatu/baoounao/domain/model/ProposalUserEntity.java
@@ -0,0 +1,10 @@
+package br.app.iftmparacatu.baoounao.domain.model;
+
+import java.time.LocalDate;
+
+public class ProposalUserEntity {
+    private Integer id;
+    private ProposalEntity proposalEntity;
+    //private Usuario user;
+    private LocalDate createdAt;
+}
