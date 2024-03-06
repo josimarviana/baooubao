576aa8d501bfc2e5c6990f9aa85a54f7703c5403
Krittz <cristianalvessilvak@gmail.com>
2024-03-06 20:33:56 -0300

    Adding SituationEntity to model package

---
+++ src/main/java/br/app/iftmparacatu/baoounao/domain/model/SituationEntity.java
@@ -0,0 +1,22 @@
+package br.app.iftmparacatu.baoounao.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
+@Entity
+public class SituationEntity {
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private Status status;
+
+    public enum Status {
+
+        OPEN_FOR_VOTING,
+        FORWARDED_TO_BOARD,
+        APPROVED,
+        DENIED,
+        PENDING_MODERATION
+    }
+}
