ff835e857289f31c92f76bb0e833482584175d96
Cristian Alves Silva <cristianalvessilvak@gmail.com>
2024-03-07 22:20:55 -0300

    add Cycle and Category repository

--- src/main/java/br/app/iftmparacatu/baoounao/domain/repository/CategoryRepository.java
+++ src/main/java/br/app/iftmparacatu/baoounao/domain/repository/CategoryRepository.java
@@ -1,9 +1,10 @@
 package br.app.iftmparacatu.baoounao.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.app.iftmparacatu.baoounao.domain.model.CategoryEntity;
-
+@Repository
 public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
 
 }
--- src/main/java/br/app/iftmparacatu/baoounao/domain/repository/CycleRepository.java
+++ src/main/java/br/app/iftmparacatu/baoounao/domain/repository/CycleRepository.java
@@ -1,9 +1,11 @@
 package br.app.iftmparacatu.baoounao.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.app.iftmparacatu.baoounao.domain.model.CycleEntity;
 
+@Repository
 public interface CycleRepository extends JpaRepository<CycleEntity, Long> {
 
 }
