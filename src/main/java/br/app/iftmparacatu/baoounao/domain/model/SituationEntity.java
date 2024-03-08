a59c854d7cd5ec87592a8ce793e7c160f5f6f767
Krittz <cristianalvessilvak@gmail.com>
2024-03-07 21:52:51 -0300

    Change SituationEntity class to ENUM

--- src/main/java/br/app/iftmparacatu/baoounao/domain/model/SituationEntity.java
+++ src/main/java/br/app/iftmparacatu/baoounao/domain/model/SituationEntity.java
@@ -1,24 +1,14 @@
 package br.app.iftmparacatu.baoounao.domain.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
-@Entity
-@Data
-public class SituationEntity {
-    @Enumerated(EnumType.STRING)
-    @Column(length = 20)
-    private Status status;
-
-    public enum Status {
-
-        OPEN_FOR_VOTING,
-        FORWARDED_TO_BOARD,
-        APPROVED,
-        DENIED,
-        PENDING_MODERATION
-    }
+@Getter
+@AllArgsConstructor
+public enum SituationEntity {
+    OPEN_FOR_VOTING,
+    FORWARDED_TO_BOARD,
+    APPROVED,
+    DENIED,
+    PENDING_MODERATION
 }
