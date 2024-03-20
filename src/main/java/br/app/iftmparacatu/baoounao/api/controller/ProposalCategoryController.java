7b7c55b168a15a7942c061690833ca4fcb10383b
Felipe Soares <soares.felipe200@gmail.com>
2024-03-20 20:21:46 -0300

    feat: adicionando get para classes associativas proposta/category e votação

--- src/main/java/br/app/iftmparacatu/baoounao/api/controller/ProposalCategoryController.java
+++ src/main/java/br/app/iftmparacatu/baoounao/api/controller/ProposalCategoryController.java
@@ -1,13 +1,16 @@
 package br.app.iftmparacatu.baoounao.api.controller;
 
 import br.app.iftmparacatu.baoounao.domain.model.ProposalCategoryEntity;
+import br.app.iftmparacatu.baoounao.domain.model.VotingEntity;
 import br.app.iftmparacatu.baoounao.domain.repository.ProposalCategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/proposal-category")
@@ -19,4 +22,8 @@ public class ProposalCategoryController {
         return proposalCategoryRepository.findAll();
     }
 
+    @GetMapping("/{proposalCategoryID}") //TODO: Adicionar exception para quando não encontrar a entidade
+    public Optional<ProposalCategoryEntity> findById(@PathVariable Long proposalCategoryID) {
+        return proposalCategoryRepository.findById(proposalCategoryID); //.orElseThrow(() -> new EntityNotFoundException("REGISTRO NÃO ENCONTRADO!"));
+    }
 }
--- src/main/java/br/app/iftmparacatu/baoounao/api/controller/VotingController.java
+++ src/main/java/br/app/iftmparacatu/baoounao/api/controller/VotingController.java
@@ -4,10 +4,12 @@ import br.app.iftmparacatu.baoounao.domain.model.VotingEntity;
 import br.app.iftmparacatu.baoounao.domain.repository.VotingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/voting")
@@ -19,4 +21,8 @@ public class VotingController {
         return votingRepository.findAll();
     }
 
+    @GetMapping("/{votingID}") //TODO: Adicionar exception para quando não encontrar a entidade
+    public Optional<VotingEntity> findById(@PathVariable Long votingID) {
+        return votingRepository.findById(votingID); //.orElseThrow(() -> new EntityNotFoundException("REGISTRO NÃO ENCONTRADO!"));
+    }
 }
