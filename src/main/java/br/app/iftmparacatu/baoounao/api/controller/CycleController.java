8021242051bc1ec084366aac2a26c38599a96b37
Felipe Soares <soares.felipe200@gmail.com>
2024-03-20 20:33:49 -0300

    feat: adicionando get único para categoria e ciclo

--- src/main/java/br/app/iftmparacatu/baoounao/api/controller/CycleController.java
+++ src/main/java/br/app/iftmparacatu/baoounao/api/controller/CycleController.java
@@ -1,13 +1,17 @@
 package br.app.iftmparacatu.baoounao.api.controller;
 
 import br.app.iftmparacatu.baoounao.domain.model.CycleEntity;
+import br.app.iftmparacatu.baoounao.domain.model.ProposalCategoryEntity;
 import br.app.iftmparacatu.baoounao.domain.repository.CycleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/cycle")
 public class CycleController {
@@ -16,5 +20,10 @@ public class CycleController {
     @GetMapping
     public List<CycleEntity> list(){
         return cycleRepository.findAll();
+    }
+
+    @GetMapping("/{cycleID}") //TODO: Adicionar exception para quando não encontrar a entidade
+    public Optional<CycleEntity> findById(@PathVariable Long cycleID) {
+        return cycleRepository.findById(cycleID); //.orElseThrow(() -> new EntityNotFoundException("REGISTRO NÃO ENCONTRADO!"));
     }
 }
