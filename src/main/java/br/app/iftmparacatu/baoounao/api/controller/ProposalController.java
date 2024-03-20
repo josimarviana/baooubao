ec54c89b7c10894271a720dc35af7c2a9b923ecc
ElisabeteXavier <elisabetebx@gmail.com>
2024-03-20 20:30:09 -0300

    feat: adicionado get por id de proposta

--- src/main/java/br/app/iftmparacatu/baoounao/api/controller/ProposalController.java
+++ src/main/java/br/app/iftmparacatu/baoounao/api/controller/ProposalController.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/proposal")
@@ -22,5 +24,15 @@ public class ProposalController {
     public ResponseEntity<List<ProposalEntity>> list (){
         List<ProposalEntity> propostas =  proposalRepository.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(propostas);
+    }
+
+    @GetMapping("/{proposalId}") //TODO: modificar a exceção quando usar o service
+    public ResponseEntity<ProposalEntity> getFindById (@PathVariable Long proposalId ){
+        Optional<ProposalEntity> proposta = proposalRepository.findById(proposalId);
+        if(proposta.isPresent()){
+            return ResponseEntity.status(HttpStatus.OK).body(proposta.get());
+
+        } else throw new RuntimeException("Proposal de id " + proposalId + "não foi encontrada");
+
     }
 }
