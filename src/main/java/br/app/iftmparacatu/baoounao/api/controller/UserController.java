0816ee7b13a70a235812f455e90c3d498620cd10
Hyan Lucas <hyanlucas2011@gmail.com>
2024-03-21 20:47:17 -0300

    feat: adicionando o metodo getUserById na classe UserController.java

--- src/main/java/br/app/iftmparacatu/baoounao/api/controller/UserController.java
+++ src/main/java/br/app/iftmparacatu/baoounao/api/controller/UserController.java
@@ -3,7 +3,10 @@ package br.app.iftmparacatu.baoounao.api.controller;
 import br.app.iftmparacatu.baoounao.domain.model.UserEntity;
 import br.app.iftmparacatu.baoounao.domain.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +21,15 @@ public class UserController {
     public List<UserEntity> list(){
         return userRepository.findAll();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserEntity> getUserById(@PathVariable Long id) {
+        UserEntity user = userRepository.findById(id).orElse(null);
+        if (user != null) {
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.notFound().build();
+    }
+}
 
 }
