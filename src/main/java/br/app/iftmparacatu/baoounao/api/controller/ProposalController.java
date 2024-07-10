219d6244eaca4ede3c23fc41ce3325135277f62b
ElisabeteXavier <elisabetebx@gmail.com>
2024-07-10 20:23:59 -0300

    post proposta

--- src/main/java/br/app/iftmparacatu/baoounao/api/controller/ProposalController.java
+++ src/main/java/br/app/iftmparacatu/baoounao/api/controller/ProposalController.java
@@ -3,13 +3,11 @@ package br.app.iftmparacatu.baoounao.api.controller;
 
 import br.app.iftmparacatu.baoounao.domain.model.ProposalEntity;
 import br.app.iftmparacatu.baoounao.domain.repository.ProposalRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -35,4 +33,17 @@ public class ProposalController {
         } else throw new RuntimeException("Proposal de id " + proposalId + "não foi encontrada");
 
     }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)//TODO: modificar a exceção quando usar o service
+    public ResponseEntity<ProposalEntity> saveProposal (@RequestBody ProposalEntity proposalEntity ){
+        try{
+            ProposalEntity proposta = proposalRepository.save(proposalEntity);
+            return ResponseEntity.status(HttpStatus.CREATED).body(proposta);
+        }catch (Exception e){
+            throw  new RuntimeException(e);
+        }
+    }
+
+
 }
