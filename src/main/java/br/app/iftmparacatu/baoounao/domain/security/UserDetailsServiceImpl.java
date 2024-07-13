a5785fcf0cb2bd529d6993974f699a34eb447ec0
josimarviana <josimarviana@gmail.com>
2024-07-13 20:27:34 -0300

    criando entidadades de seguranca

---
+++ src/main/java/br/app/iftmparacatu/baoounao/domain/security/UserDetailsServiceImpl.java
@@ -0,0 +1,19 @@
+package br.app.iftmparacatu.baoounao.domain.security;
+
+import br.app.iftmparacatu.baoounao.domain.model.UserEntity;
+import br.app.iftmparacatu.baoounao.domain.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl {
+    @Autowired
+    private UserRepository userRepository;
+    
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+        UserEntity user = userRepository.findByEmail(username).orElseThrow(() -> new RuntimeException("Usuario nao encontrado"));
+              return new UserDetailsImpl(user);
+    }
+}
