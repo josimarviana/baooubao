c8b3f0ad072f8c57ce021cfd09c82c694aa0c0e3
Felipe Soares <soares.felipe200@gmail.com>
2024-07-17 20:28:28 -0300

    refactor: removendo arquivo de variaveis de ambiente, e atualizacao de gitignore

--- src/main/java/br/app/iftmparacatu/baoounao/BaoounaoApplication.java
+++ src/main/java/br/app/iftmparacatu/baoounao/BaoounaoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:env/local.properties")
 public class BaoounaoApplication {
 
 	public static void main(String[] args) {
