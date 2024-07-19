8d6fc6795d5d32687d1779bbe2fd18e30712c971
Felipe Soares <soares.felipe200@gmail.com>
2024-07-18 21:05:10 -0300

    fix: correção erro endpoint users

--- src/main/java/br/app/iftmparacatu/baoounao/config/SecurityConfig.java
+++ src/main/java/br/app/iftmparacatu/baoounao/config/SecurityConfig.java
@@ -18,23 +18,23 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     public static final String [] ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED = {
-            "/users/login", // Url que usaremos para fazer login
-            "/users" // Url que usaremos para criar um usuário
+            "/user/login", // Url que usaremos para fazer login
+            "/user" // Url que usaremos para criar um usuário
     };
 
     // Endpoints que requerem autenticação para serem acessados
     public static final String [] ENDPOINTS_WITH_AUTHENTICATION_REQUIRED = {
-            "/users/test"
+            "/user/test"
     };
 
     // Endpoints que só podem ser acessador por usuários com permissão de cliente
     public static final String [] ENDPOINTS_CUSTOMER = {
-            "/users/test/customer"
+            "/user/test/customer"
     };
 
     // Endpoints que só podem ser acessador por usuários com permissão de administrador
     public static final String [] ENDPOINTS_ADMIN = {
-            "/users/test/administrator"
+            "/user/test/administrator"
     };
 
     @Bean
