e1fc7e84b3c933d63ea2b4f4b89b14f200c02a7c
ElisabeteXavier <elisabetebx@gmail.com>
2024-07-31 12:49:48 -0300

    feat:a dicionando atualizar parcial

--- src/main/java/br/app/iftmparacatu/baoounao/domain/services/ProposalService.java
+++ src/main/java/br/app/iftmparacatu/baoounao/domain/services/ProposalService.java
@@ -1,9 +1,12 @@
 package br.app.iftmparacatu.baoounao.domain.services;
 
+import br.app.iftmparacatu.baoounao.api.exception.ProposalException;
 import br.app.iftmparacatu.baoounao.domain.dtos.output.RecoveryProposalDto;
+import br.app.iftmparacatu.baoounao.domain.enums.Situation;
 import br.app.iftmparacatu.baoounao.domain.model.ProposalEntity;
 import br.app.iftmparacatu.baoounao.domain.repository.ProposalRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -27,5 +30,18 @@ public class ProposalService {
         return proposals.stream()
                 .map(this::mapToDto)
                 .collect(Collectors.toList());
+    }
+    public RecoveryProposalDto update(Long proposalID, ProposalEntity proposalEntity) {
+        if (proposalEntity.getSituation().equals(Situation.OPEN_FOR_VOTING)){
+            throw new ProposalException("Não é possível atualizar propostas em votação");
+        } else {
+            ProposalEntity existingProposal = proposalRepository.findById(proposalID)
+                    .orElseThrow(() -> new ProposalException("Proposta não encontrada com o ID: " + proposalID));
+
+            BeanUtils.copyProperties(proposalEntity, existingProposal, "id");
+            proposalRepository.save(existingProposal); // Certifique-se de salvar a entidade atualizada
+
+            return this.mapToDto(existingProposal);
+        }
     }
 }
