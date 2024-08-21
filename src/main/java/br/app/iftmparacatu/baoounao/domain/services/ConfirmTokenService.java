dce246dbae654715a90dd4fbedb16f440bfa258d
Felipe Soares <soares.felipe200@gmail.com>
2024-08-21 13:44:28 -0300

    teste 2

--- src/main/java/br/app/iftmparacatu/baoounao/domain/services/ConfirmTokenService.java
+++ src/main/java/br/app/iftmparacatu/baoounao/domain/services/ConfirmTokenService.java
@@ -40,9 +40,9 @@ public class ConfirmTokenService {
         return urlConfirmationEmail;
     }
     public Optional<ConfirmationTokenEntity> validation(String token) {
-        System.out.println(LocalDateTime.now());
-        System.out.println(findByToken(token).get().getExpiryDate());
-        System.out.println(findByToken(token).get().getExpiryDate().isAfter(LocalDateTime.now()));
+        System.out.printf("Data Atual: %s",LocalDateTime.now());
+        System.out.printf("Data Expiração Token: %s",findByToken(token).get().getExpiryDate());
+        System.out.println("Resultado condição: "+findByToken(token).get().getExpiryDate().isAfter(LocalDateTime.now()));
         return Optional.ofNullable(confirmationTokenRepository.findByToken(token))
                 .filter(t -> t.getExpiryDate().isAfter(LocalDateTime.now()));
     }
