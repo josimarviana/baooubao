66c4d32a06aa8fae0df4309efdb0d62f29743a40
Felipe Soares <soares.felipe200@gmail.com>
2024-08-29 20:37:16 -0300

    fix: tipo primitivo ciclo

--- src/main/java/br/app/iftmparacatu/baoounao/domain/dtos/input/CreateCycleDto.java
+++ src/main/java/br/app/iftmparacatu/baoounao/domain/dtos/input/CreateCycleDto.java
@@ -3,4 +3,4 @@ package br.app.iftmparacatu.baoounao.domain.dtos.input;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
-public record CreateCycleDto(@NotNull @NotBlank String title, @NotNull LocalDate startDate, @NotNull LocalDate finishDate, boolean active) {}
+public record CreateCycleDto(@NotNull @NotBlank String title, @NotNull LocalDate startDate, @NotNull LocalDate finishDate, Boolean active) {}
